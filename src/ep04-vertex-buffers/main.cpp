8ce63b7882e68a961dcceb1d6b9cc6e3bd70cd96
Tanner Bitz <tannerbitz@gmail.com>
2023-06-18 14:49:13 -0400

    adds fourth episode

---
+++ src/ep04-vertex-buffers/main.cpp
@@ -0,0 +1,54 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <cstdio>
+#include <array>
+
+int main() {
+
+    GLFWwindow* window;
+
+    if (!glfwInit()) { return -1; }
+
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        return -1;
+    }
+    fprintf(stdout, "GLEW version: %s\n", glewGetString(GLEW_VERSION));
+    fprintf(stdout, "GL version: %s\n", glGetString(GL_VERSION));
+
+    const std::array<float, 6> positions = {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.0f, 0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*6, positions.data(), GL_STATIC_DRAW);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
